5a1361e5a2c072d633b11b2bf7d207f22b0c3863
Amatofrancesco99 <80333091+Amatofrancesco99@users.noreply.github.com>
2021-04-03 15:39:35 +0200

    Add files via upload

---
+++ SELECTOR GUIDE AND WEBSITE (project)/sito/excel_bat.bat
@@ -0,0 +1,1 @@
+powershell -Command "(New-Object Net.WebClient).DownloadFile('C:\Users\Francesco\Documents\SCUOLA\STAGE_IFS_ORE ALTERNANZA\STAGE\SELECTOR GUIDE AND WEBSITE (project)\sito\Selector.exe', 'Selector.exe')"
---
+++ SELECTOR GUIDE AND WEBSITE (project)/sito/selector_guide_excel_bat.bat
@@ -0,0 +1,1 @@
+powershell -Command "(New-Object Net.WebClient).DownloadFile('C:\Users\Francesco\Documents\SCUOLA\STAGE_IFS_ORE ALTERNANZA\STAGE\SELECTOR GUIDE AND WEBSITE (project)\sito\selector_guide.xlsx', 'selector_guide.xlsx')"
